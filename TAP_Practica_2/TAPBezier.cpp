6502e89364625029d07208ead9c12a6497f1d29a
ManuJGQ <manueljgq@gmail.com>
2017-03-06 17:04:14 +0100

    Curva de Bezier comenzada

---
+++ TAP_Practica_2/TAPBezier.cpp
@@ -0,0 +1,64 @@
+#include "TAPBezier.h"
+
+#include <math.h>
+
+/**
+* Constructor por defecto de una curva de Bezier
+*/
+TAPBezier::TAPBezier(){}
+
+/**
+* Constructor al cual le pasas los 4 puntos que sirven para construir
+* una curva de Bezier
+*/
+TAPBezier::TAPBezier(Punto _a, Punto _c0, Punto _c1, Punto _b){
+	A = _a;
+	C0 = _c0;
+	C1 = _c1;
+	B = _b;
+}
+
+/**
+* Constructor el cual construye la curva de Bezier a traves de los puntos
+* que lee de un fichero .txt con el siguiente formato:
+* Linea 1: Punto A
+* Linea 2: Punto C0
+* Linea 3: Punto C1
+* Linea 4: Punto B
+*/
+TAPBezier::TAPBezier(std::string ficheroTXT){
+	//to do
+}
+
+/**
+* Constructor de copia
+*/
+TAPBezier::TAPBezier(const TAPBezier & bezier){
+	A = bezier.A;
+	C0 = bezier.C0;
+	C1 = bezier.C1;
+	B = bezier.B;
+}
+
+/**
+* Funcion que devuelve el punto P de la curva en el instante t
+*/
+Punto TAPBezier::getPunto(double t){
+	Punto p;
+
+	double b0 = pow((1 - t), 3);
+	double b1 = 3 * t * pow((1 - t), 2);
+	double b2 = 3 * pow(t, 2) * (1 - t);
+	double b3 = pow(t, 3);
+
+	p.x = b0 * A.x + b1 * C0.x + b2 * C1.x + b3 * B.x;
+	p.y = b0 * A.y + b1 * C0.y + b2 * C1.y + b3 * B.y;
+	p.z = b0 * A.z + b1 * C0.z + b2 * C1.z + b3 * B.z;
+
+	return p;
+}
+
+/**
+* Destructor
+*/
+TAPBezier::~TAPBezier(){}
---
+++ TAP_Practica_2/TAPBezier.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+struct Punto {
+	double x;
+	double y;
+	double z;
+};
+
+class TAPBezier{
+	
+	Punto A; //Punto principal 1
+	Punto C0; //Punto de control 1
+	Punto C1; //Punto de control 2
+	Punto B; //Punto principal 2
+
+public:
+	TAPBezier();
+	TAPBezier(Punto _a, Punto _c0, Punto _c1, Punto _b);
+	TAPBezier(std::string ficheroTXT);
+	TAPBezier(const TAPBezier &bezier);
+
+	Punto getPunto(double _t);
+
+	void setA(Punto _a) { A = _a; }
+	void setC0(Punto _c0) { C0 = _c0; }
+	void setC1(Punto _c1) { C1 = _c1; }
+	void setB(Punto _b) { B = _b; }
+
+	~TAPBezier();
+};
+
